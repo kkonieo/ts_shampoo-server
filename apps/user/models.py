c0e04b48ca4a5ee142fb1dd35bb1046a1044b528
rigyeonghong <zxcvbee@naver.com>
2022-01-28 03:58:03 +0900

    [fix] : user img null값 가능하게 수정
    - what : user table의 img 컬럼 수정
    - why : 회원가입시 사진이 없을 경우와 superuser 생성시 error 해결을 위해서

--- apps/user/models.py
+++ apps/user/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class UserManager(BaseUserManager):
     def create_user(self, name, email, job, img, password=None):
         if not name:
-            raise ValueError('must have user email')
+            raise ValueError('must have user name')
         if not email:
             raise ValueError('must have user email')
         user = self.model(
@@ -18,7 +18,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, name, email, job, img, password):
+    def create_superuser(self, name, email, job, img=None, password=None):
             user = self.create_user(
                 name=name,
                 email=self.normalize_email(email),
@@ -32,10 +32,10 @@ class UserManager(BaseUserManager):
 
 # 실제 모델을 생성하는 class
 class User(AbstractBaseUser):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=255,unique=True,primary_key=True)
+    name = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100,unique=True, primary_key=True)
     job = models.CharField(max_length=100)
-    img = models.CharField(max_length=200)
+    img = models.CharField(max_length=200, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
@@ -47,14 +47,14 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.name
 
-# 뷰 함수 내부에서 사용자가 특정 권한 갖고 있는지 확인
-def has_perm(self, perm, obj=None):
-    return True
+    # 뷰 함수 내부에서 사용자가 특정 권한 갖고 있는지 확인
+    def has_perm(self, perm, obj=None):
+        return True
 
-def has_module_perms(self, app_label):
-    return True
+    def has_module_perms(self, app_label):
+        return True
 
-# @는 권한을 뷰 함수 전체에 적용해야하는 경우
-@property
-def is_staff(self):
-    return self.is_admin
+    # @는 권한을 뷰 함수 전체에 적용해야하는 경우
+    @property
+    def is_staff(self):
+        return self.is_admin
