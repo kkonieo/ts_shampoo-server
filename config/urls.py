bdc85faf4e6a25273836c8fbbfef77d65f8833b2
nyh <dkzndk1100@naver.com>
2022-02-03 18:33:51 +0900

    [feat] : swagger 추가
    - what : swagger 추가
    - why : 원활한 협업을 위해 API를 문서화

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,38 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-urlpatterns = [path("admin/", admin.site.urls)]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Portfolio API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.orapp.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+if settings.DEBUG:
+    urlpatterns = [
+        path("admin/", admin.site.urls),
+        path(
+            "readoc/",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
+        path(
+            "",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path("user/", include("apps.user.urls"), name="user")
+    ]
