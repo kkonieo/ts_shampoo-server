4b75ed8901c8234ddcad47c9fb7b1bbfea0c9331
suzybaek <bss5770@gmail.com>
2022-02-04 18:12:06 +0900

    [fix] : merge 오류 수정
    - what : merge 오류 수정
    - why : merge 오류 발생

--- config/urls.py
+++ config/urls.py
@@ -14,19 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    
-    # path("auth/", include("apps.user.urls")),
-] 
-
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    # path("auth/", include("apps.user.urls")),
+]
 
 schema_view = get_schema_view(
     openapi.Info(
