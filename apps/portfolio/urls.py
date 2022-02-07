76487328f8d7c3a10b8b15792f674c83f9cb88d7
SeungSoo Baek <43577729+suzybaek@users.noreply.github.com>
2022-02-07 18:08:34 +0900

    [feat] : fix 및 URL링크 serializer 추가 (#25)
    - what : views update,delete DetailAPI로 합침(put, delete 요청),
    URL링크 serializer 추가함으로써 정상적으로 동작 확인(기존 nested 구조 오류 해결)
    - why : URL명, Link 저장하고 내보내기

--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -6,7 +6,5 @@ app_name = "project"
 
 urlpatterns = [
     path("<int:author>/", views.ProjectAPI.as_view(), name="project-list"),
-    path("<int:author>/<int:pk>/", views.ProjectDetailAPI.as_view(), name="project-detail"), # noqa : W503
-    path("<int:author>/<int:pk>/projectDelete", views.ProjectDeleteAPI.as_view(), name='delete'), # noqa : W503
-    path("<int:author>/<int:pk>/projectUpdate", views.ProjectUpdateAPI.as_view(), name='update'), # noqa : W503
+    path("<int:author>/<int:pk>/", views.ProjectDetailAPI.as_view(), name="project-detail"),  # noqa : W503
 ]
