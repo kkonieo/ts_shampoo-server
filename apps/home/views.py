242406cf433e99e85c8805a2d62fdc7ed2df3c96
rigyeonghong <zxcvbee@naver.com>
2022-03-24 12:52:38 +0900

    [feat] :  home api 생성
        - what : home api 생성
        - why : home api 연결을 위해

--- apps/home/views.py
+++ apps/home/views.py
@@ -5,10 +5,12 @@ from rest_framework.generics import ListAPIView
 from ..user.models import User
 from .serializers import HomeSerializer
 
+
 class HomeListAPIView(ListAPIView):
     """
     메인 페이지 모든 사용자 정보 조회
     """
 
     queryset = User.objects.all()
+
     serializer_class = HomeSerializer
