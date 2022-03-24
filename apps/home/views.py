4230fcee375b45fb6193e30912d12e6ae19e36d4
rigyeonghong <zxcvbee@naver.com>
2022-03-24 22:24:59 +0900

    [fix] : lint 오류 해결
    - what :  lint 오류 해결
    - why : 코드 가독성을 위해

--- apps/home/views.py
+++ apps/home/views.py
@@ -5,6 +5,7 @@ from rest_framework.generics import ListAPIView
 from ..user.models import User
 from .serializers import HomeSerializer
 
+
 class HomeListAPIView(ListAPIView):
     """
     메인 페이지 모든 사용자 정보 조회
