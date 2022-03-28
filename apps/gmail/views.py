f5d4ecfef5cf0e084c29aece4ab03fc6ed2be3e8
rigyeonghong <zxcvbee@naver.com>
2022-03-28 15:07:37 +0900

    [fix] : 이메일 전송 권한 설정 및 토큰으로 사용자 구분
    - what : 이메일 전송 권한 설정 및 토큰으로 사용자 구분
    - why : 보안 향상을 위해

--- apps/gmail/views.py
+++ apps/gmail/views.py
@@ -15,14 +15,14 @@ class SendEmail(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        email_send = request.data.get("email_send")
-        name_send = request.data.get("name_send")
-        content = request.data.get("content")
-        name_receive = request.data.get("name_receive")
-        email_receive = request.data.get("email_receive")
+        from_email = request.data.get("from_email")
+        from_name = request.data.get("from_name")
+        email_text = request.data.get("email_text")
+        to_email = request.user
+        to_name = request.user.name
 
-        subject = "ts-shampoo에서 {}님의 이력 보고 제안 드립니다.".format(name_receive)
-        to = [email_receive]
-        message = "안녕하세요, \nts-shampoo에서 {0}님이 {1}님의 이력을 보고 연락드립니다. \n{2}로 회신 부탁드립니다.\n\n{3}".format(name_send, name_receive, email_send, content) # noqa : E501
+        subject = "ts-shampoo에서 {}님의 이력 보고 제안 드립니다.".format(to_name)
+        to = [to_email]
+        message = "안녕하세요, \nts-shampoo에서 {0}님이 {1}님의 이력을 보고 연락드립니다. \n{2}로 회신 부탁드립니다.\n\n{3}".format(from_name, to_name, from_email, email_text) # noqa : E501
         EmailMessage(subject=subject, body=message, to=to, from_email=FROM_EMAIL).send()
         return Response({"message": "ok"})
