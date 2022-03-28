bd50da5cda9f0c2f0a3e5d492467b8ed16161ea4
rigyeonghong <zxcvbee@naver.com>
2022-03-28 14:47:20 +0900

    [feat] : flake8 적용
    - what : flake8 적용
    - why : 깔끔한 코드를 위해

--- apps/gmail/views.py
+++ apps/gmail/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 
 FROM_EMAIL = settings.base.EMAIL_HOST_USER
 
+
 class SendEmail(APIView):
     """
     이메일 전송
@@ -22,7 +23,6 @@ class SendEmail(APIView):
 
         subject = "ts-shampoo에서 {}님의 이력 보고 제안 드립니다.".format(name_receive)
         to = [email_receive]
-        message = "안녕하세요, \nts-shampoo에서 {0}님이 {1}님의 이력을 보고 연락드립니다. \n{2}로 회신 부탁드립니다.\n\n{3}".format(name_send, name_receive, email_send, content)
+        message = "안녕하세요, \nts-shampoo에서 {0}님이 {1}님의 이력을 보고 연락드립니다. \n{2}로 회신 부탁드립니다.\n\n{3}".format(name_send, name_receive, email_send, content) # noqa : E501
         EmailMessage(subject=subject, body=message, to=to, from_email=FROM_EMAIL).send()
-
         return Response({"message": "ok"})
