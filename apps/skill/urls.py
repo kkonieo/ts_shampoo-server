04a30bbdf66797273a932c38f4ce87d66ae61f82
rigyeonghong <77164776+rigyeonghong@users.noreply.github.com>
2022-04-12 12:12:46 +0900

    [feat] : user skill api by user permission and create swagger (#44)
    - what : user skill api by user permission and create swagger
    - why : non-members can be inquired, members can create, modify, delete

--- apps/skill/urls.py
+++ apps/skill/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 
-from . import views
+from .views import UserSkillListAPIView, UserSkillDetailAPIView, UserSkillAPIView, SkillListAPIView # noqa : E501
 
 urlpatterns = [
-    path("", views.skill_list),
-    path("<int:pk>", views.user_skill_detail),
+    path("", SkillListAPIView.as_view(), name="skill"),
+    path("<slug>", UserSkillListAPIView.as_view(), name="user_skill_get"),
+    path("user/", UserSkillAPIView.as_view(), name="user_skill_post"),
+    path("<int:pk>/", UserSkillDetailAPIView.as_view(), name="user_skill_update"),
 ]
